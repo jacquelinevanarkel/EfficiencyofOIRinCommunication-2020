f31144a0d3326a3558a8a49dc9c313237f221615
Jacqueline <jacquelinevanarkel@gmail.com>
2020-05-07 15:54:03 +0200

    A concept for the implementation of the model, version 2, not finished.

--- outline_implementation.py
+++ outline_implementation.py
@@ -7,103 +7,118 @@ import pandas as pd
     #Code provided by Marieke
 
 # ////////////////////////// Production //////////////////////////
-    #Input: dialogue history D, lexicon L, order n, intended referent r, turns
-    #Output: signal s, dialogue history D, turns
+class production:
+#Input: dialogue history D, lexicon L, order n, intended referent i
+#Output: signal s, dialogue history D
 
-    def production(L, n, r, D = None, turns = 0):
-        #if n=0 & D is not empty
+    # Initialization
+    def __init__(self, lexicon, intention, n=0, dialogueHistory=None):
+        self.L = lexicon
+        self.i = intention
+        self.n = n
+        self.D = dialogueHistory
+
+    def produce(self):
+        if n = 0:
+            production_literal(L, i, D)
+        else:
+            production_pragmatic(n)
+
+    def production_literal(self, L, i, D):
+        #if D is not empty
         L = conjunction(D, L)
 
         #calculate which signal s maximizes the probability, using the new lexicon if D not empty
 
         #update D
+        return s, D
 
-        turns += 1
-        return s, D, turns
+    def production_pragmatic(self, n):
+        #calculate which signal s maximizes the probability
+
+        return s
 
 
 # ////////////////////////// Interpretation //////////////////////////
-    class interpretation:
-        #Input: dialogue history D, Lexicon L, order n, observed signal s, intended referent i (in case of production (?))
-        #       threshold pragmatic reasoning n_t, turns
-        #Output: [r, huh? (--> production), n+1, r (including recursion)] --> intended referent
-        #Calling functions:
-            #conditional entropy over probability distributions
-            #conjunction
-            #production: as means of OIR
+class interpretation:
+    #Input: dialogue history D, Lexicon L, order n, observed signal s, intended referent i (in case of production (?))
+    #       threshold pragmatic reasoning n_t, turns, entropy threshold
+    #Output: [r, huh? (--> production), n+1, r (including recursion)] --> intended referent
+    #Calling functions:
+        #conditional entropy over probability distributions
+        #conjunction
+        #production: as means of OIR
 
-        # Initialization
-        def __init__(self, lexicon, signal, intention, n_t = 2, n = 0):
-            self.L = lexicon
-            self.s = signal
-            self.i = intention
-            self.n_t = n_t
-            self.n = n
+    # Initialization
+    def __init__(self, lexicon, signal, n_t = 2, n = 0, dialogueHistory=None, entropyThreshold):
+        self.L = lexicon
+        self.s = signal
+        self.n_t = n_t
+        self.n = n
+        self.D = dialogueHistory
+        self.H_t = entropyThreshold
 
-        def interpret(self, D = none):
-            if n = 0:
-                interpretation_literal(D, L, s, i)
-            else:
-                interpretation_pragmatic(L, n, s, n_t)
-
-        def interpretation_literal(D, L, s, i, turns):
-            # if D is not empty
-            s = conjunction(D, s)
-
-            #calculate posterior distribution given s and D
-
-            #calculate entropy of posterior distribution
-            H = conditional_entropy(r, s, L)
-
-            # when H low: output inferred referent
-            # output = referent
-
-            # when H high:
-            # turn to speaker --> produces new signal
-            turns += 1
-            s, D, turns = production(L, 0, i, D, turns)
-            output = interpretation_literal(D, L, s, i, turns)
-
-            turns += 1
-            return output, turns
-
-        def interpretation_pragmatic(L, n, s, n_t):
-            #calculate posterior distribution given s and L
-
-            #calculate the entropy of posterior distribution
-            H = conditional_entropy(r, s, L)
-
-            #if entropy high & n < n_t
-            interpretation(L, n+1, s, n_t)
-
-            # when entropy low or when n = n_t --> recursion till n=0
-            #interpretation(D, L, n, s)
-
-            return r
-
-        def conditional_entropy(r, s, L):
-
-            return H
-
-    #Depends on how the lexicon is structured: as a pandas dataframe? --> change accordingly
-    #So this is not correct yet, I stopped in the middle as I think it would be useful to first see the structure
-    #of the lexicon before defining this
-    def conjunction(D, L = None, s = None):
-        if L is not None:
-            for index, row in L.iterrows():
-                for s_old in D:
-                    for x in L.iloc:
-                        if s_old == 1 & L.iloc[x] == 1:
-                            L_new[x] = 1
-                        else
-                            L_new[x] = 0
+    def interpret(self):
+        if n = 0:
+            interpretation_literal(D, L, s)
         else:
+            interpretation_pragmatic(L, n, s, n_t)
+
+    def interpretation_literal(D, L, s):
+        # if D is not empty
+        s = conjunction(D, s)
+
+        #calculate posterior distribution given s and D
+
+        #calculate entropy of posterior distribution
+        H = conditional_entropy(r, s, L)
+
+        # when H < H_t: output inferred referent
+        # output = referent
+
+        # when H > H_t:
+        # turn to speaker --> output = OIR
+        output = "OIR"
+
+        return output
+
+    def interpretation_pragmatic(L, n, s, n_t):
+        #calculate posterior distribution given s and L
+
+        #calculate the entropy of posterior distribution
+        H = conditional_entropy(r, s, L)
+
+        #if H > H_t & n < n_t
+        interpretation(L, n+1, s, n_t)
+
+        # if H < H_t or when n = n_t --> recursion till n=0
+        #interpretation(D, L, n, s)
+
+        return r
+
+    def conditional_entropy(r, s, L):
+
+        return H
+
+#Depends on how the lexicon is structured: as a pandas dataframe? --> change accordingly
+#So this is not correct yet, I stopped in the middle as I think it would be useful to first see the structure
+#of the lexicon before defining this
+def conjunction(D, L = None, s = None):
+    if L is not None:
+        for index, row in L.iterrows():
             for s_old in D:
-                    if s_old == 1 & s == 1:
+                for x in L.iloc:
+                    if s_old == 1 & L.iloc[x] == 1:
                         L_new[x] = 1
                     else
                         L_new[x] = 0
-        return L_new
+    else:
+        for s_old in D:
+                if s_old == 1 & s == 1:
+                    L_new[x] = 1
+                else
+                    L_new[x] = 0
+    return L_new
 
 
 
@@ -113,20 +128,41 @@ import pandas as pd
 #Initializing Agents: order of pragmatic reasoning, ambiguity level lexicon, type (listener, speaker), optional: entropy threshold
 class agent:
     #Initialization
-    def __init__(self, order, ambiguity_level, agent_type):
+    def __init__(self, order, lexicon, agentType, entropyThreshold):
         self.n = order
-        self.a = ambiguity_level
-        self.type = agent_type
+        self.L = lexicon
+        self.type = agentType
+        self.H_t = entropyThreshold
 
-Agent1 = agent(0, 1, "speaker")
-Agent2 = agent(0, 1, "listener")
+Agent1 = agent(0, 1, L, "speaker")
+Agent2 = agent(0, 1, L, "listener")
 
-#Lexicon
+#Lexicon: include ambiguity level
 
-#Intention: ? --> randomly generated?
+#Intention: randomly generated
 
+#One interaction/Communication
+def interaction(agent1, agent2):
+    turns = 0
+    if agent1.type = "speaker":
+        production().produce()
+        turns += 1
+        interpretation().interpret()
+    else:
+        #agent2 produces
+        production().produce()
+        turns += 1
+        listener_output = interpretation().interpret()
+        if listener_output == "OIR":
+            turns += 1
+            production().produce()
+        else:
+            break
+    return turns
+
+# ////////////////////////// Measurements: dependent variables //////////////////////////
 #Communicative success
-def communicative_success(i, r):
+def communicativeSuccess(i, r):
     if i == r:
         cs = 1
     else:
@@ -134,9 +170,16 @@ def communicative_success(i, r):
 
     return cs
 
-def average_com_suc():
+def averageComSuc():
     sum = 0
     for i, r in interactions:
         sum += communicative_success(i, r)
 
     return sum / len(interactions)
+
+#Complexity: also a measurement, but not included here
+
+# ////////////////////////// Running Simulations //////////////////////////
+def simulation(n_interactions, lexicon, ambiguityLevel):
+
+    return dataframe
