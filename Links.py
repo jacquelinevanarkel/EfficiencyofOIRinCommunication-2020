fbdf712b3a0a75142b5b9906ffe934cfa93cad8e
Jacqueline <jacquelinevanarkel@gmail.com>
2020-05-15 16:52:41 +0200

    Working on literal interpretation: entropy.

---
+++ Links.py
@@ -0,0 +1,18 @@
+"""" Naming conventions:
+Summary:
+https://visualgit.readthedocs.io/en/latest/pages/naming_convention.html
+Official:
+https://www.python.org/dev/peps/pep-0008/#naming-conventions
+Doc comments:
+https://www.jetbrains.com/help/pycharm/creating-documentation-comments.html
+Value error:
+https://docs.python.org/3/tutorial/errors.html
+
+Pickle:
+https://www.datacamp.com/community/tutorials/pickle-python-tutorial
+
+Zotero:
+https://retorque.re/zotero-better-bibtex/installation/
+http://zotfile.com/
+https://www.zotero.org/groups/
+"""
