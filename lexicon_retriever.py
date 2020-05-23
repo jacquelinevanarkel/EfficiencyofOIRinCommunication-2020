998b191790555ef3580a6f23c4141bc9af0518ec
Jacqueline <jacquelinevanarkel@gmail.com>
2020-05-23 18:26:15 +0200

    Debugged literal production/interpretation.

--- lexicon_retriever.py
+++ lexicon_retriever.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 lexicons_df = pd.read_json('lexiconset.json') # Make sure to specify the correct path to the json file on your machine/the cluster
 
-print("lexicons_df.head() is:")
-with pd.option_context('display.max_columns', None): # this is in order to show all the columns (instead of a truncated version)
-    print(lexicons_df.head())
+#print("lexicons_df.head() is:")
+#with pd.option_context('display.max_columns', None): # this is in order to show all the columns (instead of a truncated version)
+#    print(lexicons_df.head())
 
-print('')
-print("lexicons_df.columns is:")
-print(lexicons_df.columns)
+#print('')
+#print("lexicons_df.columns is:")
+#print(lexicons_df.columns)
 
 
 def retrieve_lex(lexicons_df, n_signals, n_referents, ambiguity_level, n_lexicons):
@@ -23,29 +23,29 @@ def retrieve_lex(lexicons_df, n_signals, n_referents, ambiguity_level, n_lexicon
     :return: 3D numpy array; axis 0 = lexicons, axis 1 = signals, axis 2 = referents
     """
     selected_lexicons = lexicons_df[lexicons_df['vocabularySize'] == n_signals][lexicons_df['contextSize'] == n_referents][lexicons_df['ambiguity'] == ambiguity_level]['lexicons'].to_numpy()
-    print('')
-    print("selected_lexicons are:")
-    with pd.option_context('display.max_columns', None):  # this is in order to show all the columns (instead of a truncated version)
-        print(selected_lexicons)
+    #print('')
+    #print("selected_lexicons are:")
+    #with pd.option_context('display.max_columns', None):  # this is in order to show all the columns (instead of a truncated version)
+    #    print(selected_lexicons)
     selected_lexicons = np.asarray(selected_lexicons[0])
-    print('')
-    print("selected_lexicons converted to numpy array are:")
-    print(selected_lexicons)
-    print("selected_lexicons.shape (intention.e. dimensions of numpy array) is:")
-    print(selected_lexicons.shape)
+    #print('')
+    #print("selected_lexicons converted to numpy array are:")
+    #print(selected_lexicons)
+    #print("selected_lexicons.shape (intention.e. dimensions of numpy array) is:")
+    #print(selected_lexicons.shape)
     random_indices = np.random.choice(np.arange(selected_lexicons.shape[0]), size=n_lexicons, replace=False) # chooses n_lexicons *unique* random integers in the range 0 to the number of lexicons in selected_lexicons
-    print('')
-    print("random_indices are:")
-    print(random_indices)
+    #print('')
+    #print("random_indices are:")
+    #print(random_indices)
     random_selection = selected_lexicons[random_indices]
-    print("random_selection is:")
-    print(random_selection)
-    print("random_selection.shape (intention.e. dimensions of numpy array) is:")
-    print(random_selection.shape)
+    #print("random_selection is:")
+    #print(random_selection)
+    #print("random_selection.shape (intention.e. dimensions of numpy array) is:")
+    #print(random_selection.shape)
     return random_selection
 
 
 
-my_lexicons = retrieve_lex(lexicons_df, 10, 8, 0.5, 5)
+#my_lexicons = retrieve_lex(lexicons_df, 10, 8, 0.5, 5)
 
 
