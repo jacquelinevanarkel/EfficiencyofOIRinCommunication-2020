95729084d083ab72cc3b55c71fea83a39f716f62
Jacqueline <jacquelinevanarkel@gmail.com>
2020-06-15 22:32:42 +0200

    Processing & plotting data of simulation 2.

--- Reading_data.py
+++ Reading_data.py
@@ -2,24 +2,23 @@
 import pandas as pd
 from pathlib import Path
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+import pickle
 
 # ---------------------------------- Reading in data & preprocessing before plotting -----------------------------------
 # Specify the directory in which you stored the results
-entries = Path('/Users/Jacqueline/Documents/Research_Internship/Implementation/Simulations/Results/Simulations/')
+entries = Path('/Users/Jacqueline/Documents/Research_Internship/Implementation/Simulations/Simulation2/Simulation2')
 
 # Initialize pandas dataframe to store results
 results = pd.DataFrame(
     columns=['Intention Speaker', 'Inferred Referent Listener', 'Number of Turns', 'Order of Reasoning Speaker',
              'Order of Reasoning Listener', 'Communicative Success', 'Reached Threshold Order',
-             'Reached Threshold Interaction', 'Dialogue History', 'Ambiguity Level', 'Number of Signals',
-             'Number of Referents', 'Entropy Threshold'])
+             'Reached Threshold Interaction', 'Dialogue History', 'Entropy', 'Probability Dist. Given Signal',
+             'Ambiguity Level', 'Number of Signals', 'Number of Referents', 'Entropy Threshold'])
 
 # Read in all files of the directory in which you stored the results and concatenate them in order to visualise
 for entry in entries.iterdir():
-    if str(entry).startswith('/Users/Jacqueline/Documents/Research_Internship/Implementation/Simulations/Results/'
-                             'Simulations/amb'):
+    if str(entry).startswith('/Users/Jacqueline/Documents/Research_Internship/Implementation/Simulations/Simulation2/'
+                             'Simulation2/amb'):
         result = pd.read_pickle(entry)
         results = pd.concat([results, result])
 
@@ -29,47 +28,25 @@ results["Number of Turns"] = results["Number of Turns"].astype(int)
 results["Ambiguity Level"] = results["Ambiguity Level"].astype(float)
 results["Number of Signals"] = results["Number of Signals"].astype(int)
 
+# Replace the entropy threshold for pragmatic agents with order 2 with NaN as it's not applicable
+results.loc[results["Order of Reasoning Listener"] == 2, "Entropy Threshold"] = np.nan
+
 # Take the results of the interactional and pragmatic agents separately
 results_interactional = results[results["Order of Reasoning Listener"] == 0]
 results_pragmatic = results[results["Order of Reasoning Listener"] != 0]
 
-# Replace the entropy threshold for pragmatic agents with order 2 with NaN as it's not applicable
-results.loc[results["Order of Reasoning Listener"] == 2, "Entropy Threshold"] = np.nan
+# Pickle the results to be stored and used later for plotting
+filename = 'results.p'
+outfile = open(filename, 'wb')
+pickle.dump(results, outfile)
+outfile.close()
 
-# ------------------------------------------------- Plot the results ---------------------------------------------------
+filename = 'results_interactional.p'
+outfile = open(filename, 'wb')
+pickle.dump(results_interactional, outfile)
+outfile.close()
 
-# Plot the communicative success per lexicon size with lines for the chance levels & relative CS
-CS_means = results.groupby("Number of Referents")["Communicative Success"].mean()
-
-plt.subplot(1, 2, 1)
-Bars = plt.bar(np.arange(3), CS_means, align='center')
-_ = plt.xticks(np.arange(3), ["4", "10", "20"])
-plt.ylim(0, 1)
-plt.title("CS for Different Lexicon Sizes")
-plt.ylabel("Communicative Success")
-plt.xlabel("Number of Referents Lexicon")
-
-chance_levels = (0.25, 0.1, 0.05)
-x_start = np.array([plt.getp(item, 'x') for item in Bars])
-x_end = x_start+[plt.getp(item, 'width') for item in Bars]
-
-plt.hlines(chance_levels, x_start, x_end)
-
-plt.subplot(1, 2, 2)
-Bars = plt.bar(np.arange(3), CS_means-chance_levels, align='center')
-_ = plt.xticks(np.arange(3), ["4", "10", "20"])
-plt.ylim(0, 1)
-plt.title("CS for Different Lexicon Sizes - Chance Levels per Lexicon Size")
-plt.ylabel("Relative Communicative Success")
-plt.xlabel("Number of Referents Lexicon")
-
-# Used for other plots
-
-#fig, ax = plt.subplots(1,2)
-#sns.barplot(x="Entropy Threshold", y="Number of Turns" , data=results, hue="Order of Reasoning Listener", ax=ax[0])
-#sns.barplot(x="Entropy Threshold", y="Communicative Success", data=results, hue="Order of Reasoning Listener", ax=ax[1)
-
-#sns.catplot(x="Ambiguity Level", y="Communicative Success", hue="Number of Signals", col="Entropy Threshold",
-#            kind="bar", data=results_pragmatic)
-
-plt.show()
+filename = 'results_pragmatic.p'
+outfile = open(filename, 'wb')
+pickle.dump(results_pragmatic, outfile)
+outfile.close()
---
+++ plotting.py
@@ -0,0 +1,250 @@
+# Necessary imports
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# Read in data
+results = pd.read_pickle("results.p")
+results_interactional = pd.read_pickle("results_interactional.p")
+results_pragmatic = pd.read_pickle("results_pragmatic.p")
+
+# ------------------------------------------------- Plot the results ---------------------------------------------------
+# Plot the communicative success per lexicon size with lines for the chance levels & relative CS
+CS_means_interactional = results_interactional.groupby(["Number of Referents","Ambiguity Level"])["Communicative Success"].mean()
+CS_means_pragmatic = results_pragmatic.groupby(["Number of Referents","Ambiguity Level"])["Communicative Success"].mean()
+
+# Calculate SDs
+SDs = []
+size = results_interactional.groupby(["Number of Referents", "Ambiguity Level"]).size()
+for index, value in CS_means_interactional.items():
+    P = value
+    N = size[index]
+    SD = ((P*(1-P))/N)**0.5
+    SDs.append(SD)
+
+plt.subplot(1, 2, 1)
+# set width of bar
+barWidth = 0.25
+
+# set height of bar
+bars1 = CS_means_interactional[:3]
+bars2 = CS_means_interactional[3:6]
+bars3 = CS_means_interactional[6:9]
+
+# Set position of bar on X axis
+r1 = np.arange(len(bars1))
+r2 = [x + barWidth for x in r1]
+r3 = [x + barWidth for x in r2]
+
+# Make the plot
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4', yerr=[x * 2 for x in SDs[:3]])
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10', yerr=[x * 2 for x in SDs[3:6]])
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20', yerr=[x * 2 for x in SDs[6:9]])
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Ambiguity Level', fontweight='bold')
+plt.ylabel('Communicative Success', fontweight='bold')
+plt.xticks([r + barWidth for r in range(len(bars1))], ['0.2','0.5','0.8'])
+
+plt.suptitle("Communicative Success for Different Ambiguity levels and Lexicon Sizes")
+plt.title("Interactional Agents")
+
+# Add chance levels
+chance_levels = (0.25, 0.1, 0.05, 0.25, 0.1, 0.05, 0.25, 0.1, 0.05)
+x_start = [r1[0]-(barWidth/2), r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(barWidth/2), r2[1]-(barWidth/2),
+           r3[1]-(barWidth/2), r1[2]-(barWidth/2), r2[2]-(barWidth/2), r3[2]-(barWidth/2)]
+x_end = [r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(1.5*barWidth), r2[1]-(barWidth/2), r3[1]-(barWidth/2),
+         r1[2]-(1.5*barWidth), r2[2]-(barWidth/2), r3[2]-(barWidth/2), r3[2]+barWidth-(barWidth/2)]
+plt.hlines(chance_levels, x_start, x_end)
+
+# Start second plot
+plt.subplot(1, 2, 2)
+
+# Calculate SDs
+SDs = []
+size = results_pragmatic.groupby(["Number of Referents", "Ambiguity Level"]).size()
+for index, value in CS_means_pragmatic.items():
+    P = value
+    N = size[index]
+    SD = ((P*(1-P))/N)**0.5
+    SDs.append(SD)
+
+# set width of bar
+barWidth = 0.25
+
+# set height of bar
+bars1 = CS_means_pragmatic[:3]
+bars2 = CS_means_pragmatic[3:6]
+bars3 = CS_means_pragmatic[6:9]
+
+# Set position of bar on X axis
+r1 = np.arange(len(bars1))
+r2 = [x + barWidth for x in r1]
+r3 = [x + barWidth for x in r2]
+
+# Make the plot
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4', yerr=[x * 2 for x in SDs[:3]])
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10', yerr=[x * 2 for x in SDs[3:6]])
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20', yerr=[x * 2 for x in SDs[6:9]])
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Ambiguity Level', fontweight='bold')
+plt.ylabel('Communicative Success', fontweight='bold')
+plt.xticks([r + barWidth for r in range(len(bars1))], ['0.2','0.5','0.8'])
+
+plt.title("Pragmatic Agents")
+
+# Add chance levels
+chance_levels = (0.25, 0.1, 0.05, 0.25, 0.1, 0.05, 0.25, 0.1, 0.05)
+x_start = [r1[0]-(barWidth/2), r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(barWidth/2), r2[1]-(barWidth/2),
+           r3[1]-(barWidth/2), r1[2]-(barWidth/2), r2[2]-(barWidth/2), r3[2]-(barWidth/2)]
+x_end = [r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(1.5*barWidth), r2[1]-(barWidth/2), r3[1]-(barWidth/2),
+         r1[2]-(1.5*barWidth), r2[2]-(barWidth/2), r3[2]-(barWidth/2), r3[2]+barWidth-(barWidth/2)]
+plt.hlines(chance_levels, x_start, x_end)
+
+# Create legend & Show graphic
+plt.legend(title="Lexicon Size")
+plt.show()
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Second plot: number of turns interactional agents
+# Plot the communicative success per lexicon size with lines for the chance levels & relative CS
+CS_means_interactional = results_interactional.groupby(["Number of Referents","Ambiguity Level"])["Number of Turns"].mean()
+CS_std_interactional = results_interactional.groupby(["Number of Referents","Ambiguity Level"])["Number of Turns"].apply(np.std)
+print(CS_std_interactional)
+
+# set width of bar
+barWidth = 0.25
+
+# set height of bar
+bars1 = CS_means_interactional[:3]
+bars2 = CS_means_interactional[3:6]
+bars3 = CS_means_interactional[6:9]
+
+# Set position of bar on X axis
+r1 = np.arange(len(bars1))
+r2 = [x + barWidth for x in r1]
+r3 = [x + barWidth for x in r2]
+
+# Make the plot
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4')
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10')
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20')
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Ambiguity Level', fontweight='bold')
+plt.ylabel('Number of Turns', fontweight='bold')
+plt.xticks([r + barWidth for r in range(len(bars1))], ['0.2','0.5','0.8'])
+
+plt.title("Number of Turns for Different Ambiguity levels and Lexicon Sizes")
+plt.legend(title="Lexicon Size")
+plt.show()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Order of reasoning on CS
+CS_means = results.groupby(["Number of Referents", "Order of Reasoning Listener"])["Communicative Success"].mean()
+
+# set width of bar
+barWidth = 0.25
+
+# set height of bar
+bars1 = CS_means[:3]
+bars2 = CS_means[3:6]
+bars3 = CS_means[6:9]
+
+# Set position of bar on X axis
+r1 = np.arange(len(bars1))
+r2 = [x + barWidth for x in r1]
+r3 = [x + barWidth for x in r2]
+
+# Make the plot
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4')
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10')
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20')
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Order of Pragmatic Reasoning', fontweight='bold')
+plt.ylabel('Communicative Success', fontweight='bold')
+plt.xticks([r + barWidth for r in range(len(bars1))], ['0','1','2'])
+
+# Add chance levels
+chance_levels = (0.25, 0.1, 0.05, 0.25, 0.1, 0.05, 0.25, 0.1, 0.05)
+x_start = [r1[0]-(barWidth/2), r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(barWidth/2), r2[1]-(barWidth/2),
+           r3[1]-(barWidth/2), r1[2]-(barWidth/2), r2[2]-(barWidth/2), r3[2]-(barWidth/2)]
+x_end = [r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(1.5*barWidth), r2[1]-(barWidth/2), r3[1]-(barWidth/2),
+         r1[2]-(1.5*barWidth), r2[2]-(barWidth/2), r3[2]-(barWidth/2), r3[2]+barWidth-(barWidth/2)]
+plt.hlines(chance_levels, x_start, x_end)
+
+plt.title("Communicative Success for Different Orders of Pragmatic Reasoning and Lexicon Sizes")
+plt.legend(title="Lexicon Size")
+plt.show()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Entropy threshold and lexicon size
+CS_means = results.groupby(["Number of Referents", "Entropy Threshold"])["Communicative Success"].mean()
+turns_means = results.groupby(["Number of Referents", "Entropy Threshold"])["Number of Turns"].mean()
+
+plt.subplot(1, 2, 1)
+# set width of bar
+barWidth = 0.25
+
+# set height of bar
+bars1 = CS_means[:3]
+bars2 = CS_means[3:6]
+bars3 = CS_means[6:9]
+
+# Set position of bar on X axis
+r1 = np.arange(len(bars1))
+r2 = [x + barWidth for x in r1]
+r3 = [x + barWidth for x in r2]
+
+# Make the plot
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4')
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10')
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20')
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Entropy Threshold', fontweight='bold')
+plt.ylabel('Communicative Success', fontweight='bold')
+plt.xticks([r + barWidth for r in range(len(bars1))], ['0.8','1.0','1.5'])
+
+# Add chance levels
+chance_levels = (0.25, 0.1, 0.05, 0.25, 0.1, 0.05, 0.25, 0.1, 0.05)
+x_start = [r1[0]-(barWidth/2), r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(barWidth/2), r2[1]-(barWidth/2),
+           r3[1]-(barWidth/2), r1[2]-(barWidth/2), r2[2]-(barWidth/2), r3[2]-(barWidth/2)]
+x_end = [r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(1.5*barWidth), r2[1]-(barWidth/2), r3[1]-(barWidth/2),
+         r1[2]-(1.5*barWidth), r2[2]-(barWidth/2), r3[2]-(barWidth/2), r3[2]+barWidth-(barWidth/2)]
+plt.hlines(chance_levels, x_start, x_end)
+
+plt.title("Communicative Success for Different Entropy Thresholds and Lexicon Sizes")
+plt.legend(title="Lexicon Size")
+
+plt.subplot(1, 2, 2)
+# set width of bar
+barWidth = 0.25
+
+# set height of bar
+bars1 = turns_means[:3]
+bars2 = turns_means[3:6]
+bars3 = turns_means[6:9]
+
+# Set position of bar on X axis
+r1 = np.arange(len(bars1))
+r2 = [x + barWidth for x in r1]
+r3 = [x + barWidth for x in r2]
+
+# Make the plot
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4')
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10')
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20')
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Entropy Threshold', fontweight='bold')
+plt.ylabel('Number of Turns', fontweight='bold')
+plt.xticks([r + barWidth for r in range(len(bars1))], ['0.8','1.0','1.5'])
+
+plt.title("Number of Turns for Different Entropy Thresholds and Lexicon Sizes")
+plt.legend(title="Lexicon Size")
+plt.show()
