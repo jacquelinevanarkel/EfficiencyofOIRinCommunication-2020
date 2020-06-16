eaddd79a495b98d17777e7ece0a0b5e9929b4b3b
Jacqueline <jacquelinevanarkel@gmail.com>
2020-06-16 14:04:25 +0200

    Processing & plotting data of simulation 2.

--- plotting.py
+++ plotting.py
@@ -11,7 +11,7 @@ results_pragmatic = pd.read_pickle("results_pragmatic.p")
 
 # ------------------------------------------------- Plot the results ---------------------------------------------------
 # Plot the communicative success per lexicon size with lines for the chance levels & relative CS
-CS_means_interactional = results_interactional.groupby(["Number of Referents","Ambiguity Level"])["Communicative Success"].mean()
+CS_means_interactional = results_interactional.groupby(["Number of Referents", "Ambiguity Level"])["Communicative Success"].mean()
 CS_means_pragmatic = results_pragmatic.groupby(["Number of Referents","Ambiguity Level"])["Communicative Success"].mean()
 
 # Calculate SDs
@@ -108,20 +108,18 @@ plt.legend(title="Lexicon Size")
 plt.show()
 
 # ---------------------------------------------------------------------------------------------------------------------
-
 # Second plot: number of turns interactional agents
 # Plot the communicative success per lexicon size with lines for the chance levels & relative CS
-CS_means_interactional = results_interactional.groupby(["Number of Referents","Ambiguity Level"])["Number of Turns"].mean()
-CS_std_interactional = results_interactional.groupby(["Number of Referents","Ambiguity Level"])["Number of Turns"].apply(np.std)
-print(CS_std_interactional)
+turns_means_interactional = results_interactional.groupby(["Number of Referents", "Ambiguity Level"])["Number of Turns"].mean()
+turns_sd_interactional = results_interactional.groupby(["Number of Referents", "Ambiguity Level"])["Number of Turns"].std()
 
 # set width of bar
 barWidth = 0.25
 
 # set height of bar
-bars1 = CS_means_interactional[:3]
-bars2 = CS_means_interactional[3:6]
-bars3 = CS_means_interactional[6:9]
+bars1 = turns_means_interactional[:3]
+bars2 = turns_means_interactional[3:6]
+bars3 = turns_means_interactional[6:9]
 
 # Set position of bar on X axis
 r1 = np.arange(len(bars1))
@@ -129,9 +127,9 @@ r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
 
 # Make the plot
-plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4')
-plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10')
-plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20')
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4', yerr=2*turns_sd_interactional[:3])
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10', yerr=2*turns_sd_interactional[3:6])
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20', yerr=2*turns_sd_interactional[6:9])
 
 # Add xticks on the middle of the group bars
 plt.xlabel('Ambiguity Level', fontweight='bold')
@@ -145,6 +143,15 @@ plt.show()
 # ----------------------------------------------------------------------------------------------------------------------
 # Order of reasoning on CS
 CS_means = results.groupby(["Number of Referents", "Order of Reasoning Listener"])["Communicative Success"].mean()
+
+# Calculate SDs
+SDs = []
+size = results.groupby(["Number of Referents", "Order of Reasoning Listener"])["Communicative Success"].size()
+for index, value in CS_means.items():
+    P = value
+    N = size[index]
+    SD = ((P*(1-P))/N)**0.5
+    SDs.append(SD)
 
 # set width of bar
 barWidth = 0.25
@@ -160,13 +167,14 @@ r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
 
 # Make the plot
-plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4')
-plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10')
-plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20')
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4', yerr=[x * 2 for x in SDs[:3]])
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10', yerr=[x * 2 for x in SDs[3:6]])
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20', yerr=[x * 2 for x in SDs[6:9]])
 
 # Add xticks on the middle of the group bars
 plt.xlabel('Order of Pragmatic Reasoning', fontweight='bold')
 plt.ylabel('Communicative Success', fontweight='bold')
+plt.ylim(0,1)
 plt.xticks([r + barWidth for r in range(len(bars1))], ['0','1','2'])
 
 # Add chance levels
@@ -183,10 +191,22 @@ plt.show()
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Entropy threshold and lexicon size
-CS_means = results.groupby(["Number of Referents", "Entropy Threshold"])["Communicative Success"].mean()
+CS_means = results_interactional.groupby(["Number of Referents", "Entropy Threshold"])["Communicative Success"].mean()
+CS_means_pragmatic = results_pragmatic.groupby(["Number of Referents", "Entropy Threshold"])["Communicative Success"].mean()
 turns_means = results.groupby(["Number of Referents", "Entropy Threshold"])["Number of Turns"].mean()
+turns_sd = results.groupby(["Number of Referents", "Entropy Threshold"])["Number of Turns"].std()
 
 plt.subplot(1, 2, 1)
+
+# Calculate SDs
+SDs = []
+size = results_interactional.groupby(["Number of Referents", "Entropy Threshold"])["Communicative Success"].size()
+for index, value in CS_means.items():
+    P = value
+    N = size[index]
+    SD = ((P*(1-P))/N)**0.5
+    SDs.append(SD)
+
 # set width of bar
 barWidth = 0.25
 
@@ -201,13 +221,14 @@ r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
 
 # Make the plot
-plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4')
-plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10')
-plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20')
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4', yerr=[x * 2 for x in SDs[:3]])
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10', yerr=[x * 2 for x in SDs[3:6]])
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20', yerr=[x * 2 for x in SDs[6:9]])
 
 # Add xticks on the middle of the group bars
 plt.xlabel('Entropy Threshold', fontweight='bold')
 plt.ylabel('Communicative Success', fontweight='bold')
+plt.ylim(0,1)
 plt.xticks([r + barWidth for r in range(len(bars1))], ['0.8','1.0','1.5'])
 
 # Add chance levels
@@ -218,10 +239,57 @@ x_end = [r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(1.5*barWidth), r2[1]-(ba
          r1[2]-(1.5*barWidth), r2[2]-(barWidth/2), r3[2]-(barWidth/2), r3[2]+barWidth-(barWidth/2)]
 plt.hlines(chance_levels, x_start, x_end)
 
-plt.title("Communicative Success for Different Entropy Thresholds and Lexicon Sizes")
+plt.suptitle("Communicative Success for Different Entropy Thresholds and Lexicon Sizes")
+plt.title("Interactional Agents")
 plt.legend(title="Lexicon Size")
 
 plt.subplot(1, 2, 2)
+
+# Calculate SDs
+SDs = []
+size = results_pragmatic.groupby(["Number of Referents", "Entropy Threshold"])["Communicative Success"].size()
+for index, value in CS_means_pragmatic.items():
+    P = value
+    N = size[index]
+    SD = ((P*(1-P))/N)**0.5
+    SDs.append(SD)
+
+# set width of bar
+barWidth = 0.25
+
+# set height of bar
+bars1 = CS_means_pragmatic[:3]
+bars2 = CS_means_pragmatic[3:6]
+bars3 = CS_means_pragmatic[6:9]
+
+# Set position of bar on X axis
+r1 = np.arange(len(bars1))
+r2 = [x + barWidth for x in r1]
+r3 = [x + barWidth for x in r2]
+
+# Make the plot
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4', yerr=[x * 2 for x in SDs[:3]])
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10', yerr=[x * 2 for x in SDs[3:6]])
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20', yerr=[x * 2 for x in SDs[6:9]])
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Entropy Threshold', fontweight='bold')
+plt.ylabel('Communicative Success', fontweight='bold')
+plt.ylim(0,1)
+plt.xticks([r + barWidth for r in range(len(bars1))], ['0.8','1.0','1.5'])
+
+# Add chance levels
+chance_levels = (0.25, 0.1, 0.05, 0.25, 0.1, 0.05, 0.25, 0.1, 0.05)
+x_start = [r1[0]-(barWidth/2), r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(barWidth/2), r2[1]-(barWidth/2),
+           r3[1]-(barWidth/2), r1[2]-(barWidth/2), r2[2]-(barWidth/2), r3[2]-(barWidth/2)]
+x_end = [r2[0]-(barWidth/2), r3[0]-(barWidth/2), r1[1]-(1.5*barWidth), r2[1]-(barWidth/2), r3[1]-(barWidth/2),
+         r1[2]-(1.5*barWidth), r2[2]-(barWidth/2), r3[2]-(barWidth/2), r3[2]+barWidth-(barWidth/2)]
+plt.hlines(chance_levels, x_start, x_end)
+
+plt.title("Pragmatic Agents")
+plt.legend(title="Lexicon Size")
+plt.show()
+# ----------------------------------------------------------------------------------------------------------------------
 # set width of bar
 barWidth = 0.25
 
@@ -236,9 +304,9 @@ r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
 
 # Make the plot
-plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4')
-plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10')
-plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20')
+plt.bar(r1, bars1, color='#ABABFF', width=barWidth, edgecolor='white', label='6x4', yerr=2*turns_sd[:3])
+plt.bar(r2, bars2, color='#5757DF', width=barWidth, edgecolor='white', label='15x10', yerr=2*turns_sd[3:6])
+plt.bar(r3, bars3, color='#000077', width=barWidth, edgecolor='white', label='30x20', yerr=2*turns_sd[6:9])
 
 # Add xticks on the middle of the group bars
 plt.xlabel('Entropy Threshold', fontweight='bold')
@@ -248,3 +316,6 @@ plt.xticks([r + barWidth for r in range(len(bars1))], ['0.8','1.0','1.5'])
 plt.title("Number of Turns for Different Entropy Thresholds and Lexicon Sizes")
 plt.legend(title="Lexicon Size")
 plt.show()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Entropy over time (turns)
